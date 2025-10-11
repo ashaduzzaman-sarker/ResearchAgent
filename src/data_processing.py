8e8eb464aacaae1d667b08650af0ad4dcf8ec72e
Ashaduzzaman Sarker <ashaduzzaman2505@gmail.com>
2025-10-11 17:12:02 +0000

    data preprocessing completed

--- src/data_processing.py
+++ src/data_processing.py
@@ -2,7 +2,7 @@ import os
 import logging
 import pandas as pd
 from tqdm import tqdm
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import yaml
 
